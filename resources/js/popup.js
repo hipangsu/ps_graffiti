311154e6ff602e71c7ce72ba36624b1f3f06b8a0
hipangu <hipangsu@gmail.com>
2022-12-02 18:02:57 +0900

    1

---
+++ resources/js/popup.js
@@ -0,0 +1,30 @@
+document.ready(function(){
+
+
+$modal = $('.modal-wrap');
+
+    function closeModal() {
+        $modal.removeClass('active');
+        $modal.addClass('leave');
+    }
+
+    $('.modal-popup').click(function() {
+      $modal.toggleClass('active');
+      $modal.removeClass('leave');
+    })
+
+    $('.modal-overlay').click(function() {
+      closeModal();
+    })
+
+    $('.close').click(function() {
+        closeModal();
+    })
+
+    // keyCode 27 = ESC
+      $(document).keyup(function(e) {
+        if(e.which === 27) {
+          closeModal();
+        }
+    })
+});
