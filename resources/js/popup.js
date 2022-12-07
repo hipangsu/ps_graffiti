1eebd4980b181076e9a3fab518c413157ece314c
hipangu <hipangsu@gmail.com>
2022-12-07 13:15:30 +0900

    1

--- resources/js/popup.js
+++ resources/js/popup.js
@@ -1,34 +1,25 @@
 $modal = $('.modal-wrap');
 
-    function closeModal() {
-        $modal.removeClass('active');
-        $modal.addClass('leave');
-    }
+function closeModal() {
+    $modal.removeClass('active');
+}
 
-    $('.modal-popup').click(function() {
-      $modal.toggleClass('active');
-      $modal.removeClass('leave');
-    })
+$('.modal-popup').click(function() {
+  $modal.toggleClass('active');
+})
 
-    $('.modal-overlay').click(function() {
+$('.modal-overlay').click(function() {
+  closeModal();
+})
+
+$('.close').click(function() {
+    closeModal();
+})
+
+// keyCode 27 = ESC
+  $(document).keyup(function(e) {
+    if(e.which === 27) {
       closeModal();
-    })
-
-    $('.close').click(function() {
-        closeModal();
-    })
-
-    // keyCode 27 = ESC
-      $(document).keyup(function(e) {
-        if(e.which === 27) {
-          closeModal();
-        }
-    })
-
-
-    var myModal = function() {
-      o.btn = null,
-
-      
-
     }
+})
+
